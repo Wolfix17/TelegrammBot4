b42a49b99ff12679b1da8cd5f9bde44584203e9f
Wolfix17 <75612884+Wolfix17@users.noreply.github.com>
2020-12-21 11:46:20 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,81 @@
+import aiogram.untils.markdown as md 
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ParseMode
+from aiogram.untils import executor
+
+API_TOKEN = '1433262895:AAHUkLKzEkhE5_FilAj1onoOenFSkQZA7RM'
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+
+dp = Dispatcher(bot, storage)
+
+class From(StatesGroup):
+    firstname = State()
+    lastname = State()
+    group = State()
+    startedu = State()
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    await From.firstname.set()
+    await message.reply("Привет! Как тебя зовут?")
+
+@dp.message_handler(state=From.firstname)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['firstname'] = message.text
+    await From.next()
+    await message.reply("Какая у тебя фамилия?")
+
+        
+
+@dp.message_handler(state=From.lastname)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['lastname'] = message.text
+    await From.next()
+    await message.reply("В какой группе ты учишься?")
+
+@dp.message_handler(lambda message: message.text, state=From.group)
+async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['group'] = message.text
+    await From.next()
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add("2017", "2018", "2019", "2020")
+
+    await message.reply("В каком году ты поступил?", reply_markup=markup)
+
+@dp.message_handler(lambda message: message.text not in ["2017", "2018", "2019", "2020"], state=From.startedu)
+async def process_gender_invalid(message: types.Message):
+    return await message.reply("Ответ неверный. Выберите нужный вариант на клавиатуре.")
+
+@dp.message_handler(state=From.startedu)
+async def process_gender(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['startedu'] = message.text
+
+        markup = types.ReplyKeyboardRemove()
+
+        await bot.send_message(
+            message.chat.id,
+            md.text(
+                md.text('Рад познакомиться!, ', md.bold(data['firstname'] + " " + data['lastname'])),
+                md.text('Твоя группа:', data['group']),
+                md.text('Начал обучение в: ', data['startedu']),
+                sep='\n'
+            ),
+            reply_markup=markup,
+            parse_mode=ParseMode.MARKDOWN
+        )
+    await state.finish()    
+
+    if __name__ == '__main__':
+        executor.start_polling(dp, skip_updates=True)
+    
